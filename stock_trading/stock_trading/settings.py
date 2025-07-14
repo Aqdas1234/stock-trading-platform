d4bd47302a9b09736b83fdecd3e7eb8661ef4c61
aqdas1234 <bsef21m505@pucit.edu.pk>
2025-07-14 17:27:02 +0500

    Implement rate limiting to protect APIs from abuse

--- stock_trading/stock_trading/settings.py
+++ stock_trading/stock_trading/settings.py
@@ -158,6 +158,15 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
+
+    'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.UserRateThrottle',
+        'rest_framework.throttling.AnonRateThrottle',
+    ],
+    'DEFAULT_THROTTLE_RATES': {
+        'user': '100/minute',    # Authenticated users
+        'anon': '10/minute',     # Unauthenticated users
+    }
 }
 
 
